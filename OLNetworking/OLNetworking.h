84d1cd37ce5f05ac864c26e7f628c687f94596a0
Oleg L <oleg.l@smatym.pro>
2019-02-22 10:10:22 +0600

    Initial Commit

---
+++ OLNetworking/OLNetworking.h
@@ -0,0 +1,19 @@
+//
+//  OLNetworking.h
+//  OLNetworking
+//
+//  Created by Олег Лейзер on 22/02/2019.
+//  Copyright © 2019 Олег Лейзер. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OLNetworking.
+FOUNDATION_EXPORT double OLNetworkingVersionNumber;
+
+//! Project version string for OLNetworking.
+FOUNDATION_EXPORT const unsigned char OLNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OLNetworking/PublicHeader.h>
+
+
